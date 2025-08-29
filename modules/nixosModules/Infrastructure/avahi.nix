fdbc57d4619541b29b18b9d98dfb5ba4583ea3c8
wsdlly02 <wsdlly02@hotmail.com>
2025-08-29 23:58:06 +0800

    revise firewall rules & avahi

--- modules/nixosModules/Infrastructure/avahi.nix
+++ modules/nixosModules/Infrastructure/avahi.nix
@@ -5,15 +5,22 @@
       enable = true;
       publish = {
         enable = true;
+        addresses = true;
+        domain = true;
         userServices = true;
       };
       nssmdns6 = true;
       nssmdns4 = true;
       ipv6 = true;
       ipv4 = true;
+      wideArea = false;
       extraConfig = ''
         [server]
         disallow-other-stacks=yes
+
+        [publish]
+        publish-aaaa-on-ipv4=yes
+        publish-a-on-ipv6=yes
       '';
     };
   };
